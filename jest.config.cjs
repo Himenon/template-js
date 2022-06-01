4e99203a3466f337751cbb3707967761b34b3595
Himenon <k.himeno314@gmail.com>
2022-06-01 16:29:13 +0900

    chore: ext

---
+++ jest.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  automock: false,
+  roots: ["<rootDir>/src"], 
+  testEnvironment: "node",
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
+  transform: {
+    "^.+\\.(t|j)sx?$": "@swc/jest",
+  },
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
+  transformIgnorePatterns: [],
+};
